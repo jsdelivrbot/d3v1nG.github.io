013d2a10800bfea1f24bcbfee6645a4bfd1b3aa6
Devin Gluesing <devglue18@bold.k12.mn.us>
2018-01-04 23:23:24 +0000

    idek

--- game.js
+++ game.js
@@ -1,10 +1,21 @@
 var player;
+var playerImage;
 var enemy;
+var enemyImage;
+var isGameOver;
+
+function preload() {
+    playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png")
+    enemyImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/OdL0XPt.png")
+}
 
 function setup() {
+    isGameOver = false;
     createCanvas(250, 250);
-    player = createSprite(width/2, height-25, 50, 50);
-    enemy = createSprite(width/2, 0, 10, 30)
+    player = createSprite(width/2, height-25, 0, 0);
+    player.addImage(playerImage)
+    enemy = createSprite(width/2, 0, 0, 0)
+    enemy.addImage(enemyImage)
 }
 
 function draw() {
@@ -21,9 +32,38 @@ function draw() {
     
     if (enemy.position.y > height) {
         enemy.position.y = 0;
-        enemy.position.x = random()
+        enemy.position.x = random(5, width-5);
+    }
+    
+    if (enemy.overlap(player)) {
+        gameOver();
+    }
+    
+    if (isGameOver) {
+        gameOver();
+    } else {
+        if (enemy.overlap(player)){
+            isGameOver = true;
+        }
     }
     
     drawSprites();
-    
+}
+
+function gameOver() {
+    background(0);
+    textAlign(CENTER);
+    fill("white");
+    text("Game Over!", width/2, height/2);
+    text("Click anywhere to try again.", width/2, 3*height/4);
+}
+
+function mouseClicked() {
+    if (isGameOver) {
+        isGameOver = false;
+        player.position.x = width/2;
+        player.position.y = height-25;
+        enemy.position.x = width/2;
+        enemy.position.y = 0;
+    }
 }
