901121f7fcc4aacf7bb79e86eaa26b50ebb3aa5f
Devin Gluesing <devglue18@bold.k12.mn.us>
2018-01-05 14:02:09 +0000

    6

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ function preload() {
 
 function setup() {
     isGameOver = false;
-    createCanvas(250, 250);
+    createCanvas(256, 256);
     player = createSprite(width/2, height-25, 0, 0);
     player.addImage(playerImage)
     enemy = createSprite(width/2, 0, 0, 0)
