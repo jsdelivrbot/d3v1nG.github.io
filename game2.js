9fa2cce1c4630cc7610e582b7647ef774078b75e
Devin Gluesing <devglue18@bold.k12.mn.us>
2018-01-23 15:58:47 +0000

    added images

--- game2.js
+++ game2.js
@@ -4,9 +4,18 @@ var player1Image;
 var player2;
 var player2Image;
 
+function preload() {
+    player1Image = loadImage("./Images/rick.png")
+    player2Image = loadImage("./Images/vader.png")
+}
+
 function setup() {
-    createCanvas(1000, 750)
+    createCanvas(1425, 800)
     background(100)
     
-    player1 = createSprite()
+    player1 = createSprite(0, 0, 200, 200)
+    player1.addImage(player1Image);
+    
+    player2 = createSprite()
+    player2.addImage(player2Image);
 }
